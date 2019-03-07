2a6497ca87dd96d054de4749b5a5d731e051455a
giuliano.arend <giuliano.arend@kompetentia.fr>
2019-03-07 17:03:12 +0100

    Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {TeamListComponent} from './team-list/team-list.component'
 import {MatchListComponent} from './match-list/match-list.component'
+import {MyDashboardComponent} from './my-dashboard/my-dashboard.component'
 
 const routes: Routes = [
+  { path: '', component: MyDashboardComponent },
   { path: 'teams', component: TeamListComponent },
   { path: 'matches', component: MatchListComponent }
 ];
